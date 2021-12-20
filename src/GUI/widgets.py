54397a465ad3e6d044dec65daf537ba9723d7fa4
panos23kar <panos23kar@gmail.com>
2021-12-20 22:11:57 +0100

    create_label fucntion creates and places label

--- src/GUI/widgets.py
+++ src/GUI/widgets.py
@@ -6,5 +6,14 @@ import tkinter as tk
 def create_menu(master, tearoff=None):
     """
     Creates tkinter menu
+    |
     """
     return tk.Menu(master, tearoff=tearoff)
+
+def create_label(master, text, x, y, font="Helvetica 10"):
+    """
+    Creates a Label widget
+    """
+    label = tk.Label(master, text=text, font=font)
+    label.place(x=x, y=y)
+    return 
