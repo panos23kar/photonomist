33ba5d3217138fe5eaf0ae16e80ec573055dea83
panos23kar <panos23kar@gmail.com>
2021-12-20 22:13:16 +0100

    __info_window and __title methods for info window

--- src/GUI/info.py
+++ src/GUI/info.py
@@ -5,6 +5,10 @@ Hosts the code for the info window
 import tkinter as tk
 import webbrowser
 
+from languages import en
+
+from widgets import create_label
+
 
 class Info():
     """
@@ -23,6 +27,25 @@ class Info():
         Shows the info window
         |
         """
+        self.__info_window()
 
         # Gets the 'full' focus of the app
-        self.__info_window.grab_set()
+        self.__info.grab_set()
+
+        self.__title()
+
+    def __info_window(self):
+        """
+        Title and dimensions for the info window
+        |
+        """
+        self.__info = tk.Toplevel(self.__main_window)
+        self.__info.title(en.INFO_WINDOW_TITLE)
+        self.__info.geometry("640x400")
+    
+    def __title(self):
+        """
+        Draws the title for the Info window
+        |
+        """
+        self.__title_label = create_label(self.__info, text=en.INFO_TITLE, x=10, y=10, font="Helvetica 16 bold italic")
--- src/GUI/languages/en.py
+++ src/GUI/languages/en.py
@@ -1,3 +1,4 @@
+# Main Window
 MAIN_TITLE = "Photonomist"
 
 QUIT_MESSAGE = "Do you REALLY want to quit Photonomist?"
@@ -5,3 +6,7 @@ QUIT_MESSAGE = "Do you REALLY want to quit Photonomist?"
 MAIN_MENU_FILE = "File"
 MAIN_MENU_QUIT = "Quit"
 MAIN_MENU_INFO = "Info..."
+
+# Info window
+INFO_WINDOW_TITLE = "Photonomist Info"
+INFO_TITLE = "Photonomist"
