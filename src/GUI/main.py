0f7d34e37a0be554c876fe046dc81bb3389e3b34
panos23kar <panos23kar@gmail.com>
2021-12-20 20:24:22 +0100

    title and dimensions for main window

--- src/GUI/main.py
+++ src/GUI/main.py
@@ -3,6 +3,8 @@ Hosts the code for the main window
 """
 import tkinter as tk
 
+from languages import en
+
 class Main:
     """
     Draws the main window of the graphical user interface (GUI)
@@ -14,3 +16,13 @@ class Main:
         Initializes the main window
         |
         """
+        self.__gui = tk.Tk()
+        self.__main_window()
+    
+    def __main_window(self):
+        """
+        Title and dimensions for the main window
+        |
+        """
+        self.__gui.title("Photonomist")
+        self.__gui.geometry("440x420")
