ef291d2653db99a99efca75e5ba60a1c71e5f511
panos23kar <panos23kar@gmail.com>
2021-12-29 11:17:30 +0100

    __draws_frame, __on_mousewheel, __on_frame_configure methods for exclude window

--- src/GUI/exclude.py
+++ src/GUI/exclude.py
@@ -27,6 +27,7 @@ class Exclude:
         """
         self.__exclude_window()
         self.__draws_canvas()
+        self.__draws_frame()
 
     def __exclude_window(self):
         """
@@ -40,8 +41,35 @@ class Exclude:
 
     def __draws_canvas(self):
         """
-        Draws the canvas for the exclude window.
-        Canvas (in combination with the frame) is needed for the scrollbar.
+        Draws the canvas for the exclude window
+        Canvas, in combination with the frame, is needed for the scrollbar
         |
         """
         self.__exclude_canvas = create_canvas(self.__exclude)
+        self.__exclude_canvas.bind_all("<MouseWheel>", self.__on_mousewheel)
+
+    def __draws_frame(self):
+        """
+        Draws the frame for the which will be attached to canvas for the exclude window
+        Frame, in combination with the canvas, is needed for the scrollbar
+        |
+        """
+        self.____exclude_frame = create_frame(self.__exclude_canvas)
+        self.____exclude_frame.bind("<Configure>", lambda event, canvas=self.__exclude_canvas: self.__on_frame_configure())
+    
+    def __on_mousewheel(self, event):
+        """
+        It listens for mouse's wheel scrolling. 
+        Connected with a canvas widget.
+
+        https://stackoverflow.com/questions/17355902/tkinter-binding-mousewheel-to-scrollbar 
+        |
+        """
+        self.__exclude_canvas.yview_scroll(int(-1*(event.delta/120)), "units")
+    
+    def __on_frame_configure(self):
+        """
+        Resets the scroll region to encompass the inner frame
+        |
+        """
+        self.__exclude_canvas.configure(scrollregion=self.__exclude_canvas.bbox("all"))
