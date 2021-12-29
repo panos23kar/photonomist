fafe21b554749ea43f9ac3c14d7f8c5a9263617f
panos23kar <panos23kar@gmail.com>
2021-12-29 09:46:40 +0100

    __init__ for exclude window

--- src/GUI/exclude.py
+++ src/GUI/exclude.py
@@ -3,8 +3,15 @@ Hosts the code for the exclude window
 """
 import tkinter as tk
 
-class Exclude():
+class Exclude:
     """
     Draws the Exclude window
     |
     """
+
+    def __init__(self) -> None:
+        """
+        Initializes the exclude window
+        |
+        """
+        self.__draw_exclude_window()
