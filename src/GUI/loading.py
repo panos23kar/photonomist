443ba616ed0c042fb2879ec3ac20a16618a00928
panos23kar <panos23kar@gmail.com>
2022-01-07 17:30:42 +0100

    load_image for loading window

--- src/GUI/loading.py
+++ src/GUI/loading.py
@@ -3,10 +3,11 @@ Hosts the code for the loading window
 """
 import tkinter as tk
 
+from byte_stream import BYTESTREAM
+
 from PIL import ImageTk, Image
 from base64 import b64decode
 from io import BytesIO
-import json
 
 
 class Loading:
@@ -22,3 +23,4 @@ class Loading:
         """
         self.main_window = main_window
         self.__angle = 0
+        self.__load_image = Image.open(BytesIO(b64decode(BYTESTREAM)))
