3e23355bc40e75b0329f708abbb1edcd863e3e95
panos23kar <panos23kar@gmail.com>
2022-01-07 19:35:14 +0100

    created greek file and translate part of text

---
+++ src/GUI/languages/__pycache__/gr.py
@@ -0,0 +1,55 @@
+# Main Window
+MAIN_TITLE = "Photonomist"
+
+QUIT_MESSAGE = "Αλήθεια  τώρα? Θα με κλείσεις?"
+
+MAIN_MENU_FILE = "Αρχείο"
+MAIN_MENU_QUIT = "'Eξοδος"
+MAIN_MENU_INFO = "Πληροφορίες..."
+
+MAIN_INPUT_PATH = "Φάκελος φωτ/ιών: "
+MAIN_PATH_BUTTON = "..."
+MAIN_FIND_PHOTOS_BUTTON = "Βρες φωτ/ες"
+MAIN_EXPORT_PATH = "Φάκελος εξαγωγής: "
+MAIN_FILE_EXPLORER_MESSAGE = "Επιλέξτε φάκελο"
+
+MAIN_GROUP_BY_LABEL = "Θα τις κατατάξω με βάση .."
+MAIN_DAY_RADIO_BUTTON = "Ημέρα "
+MAIN_DAY_RADIO_BUTTON_VALUE = "ημέρα "
+MAIN_MONTH_RADIO_BUTTON = "Μήνα"
+MAIN_MONTH_RADIO_BUTTON_VALUE = "μήνα"
+MAIN_YEAR_RADIO_BUTTON = "Χρόνο"
+MAIN_YEAR_RADIO_BUTTON_VALUE = "χρόνο"
+
+MAIN_NAME_PATTERN_LABEL = "Κάντε κλικ στις ετικέτες που θέλετε να προσθέσετε στο όνομα των φακέλων φωτογραφιών σας: "
+MAIN_NAME_PATTERN_PLACE = "_που"
+MAIN_NAME_PATTERN_REASON = "_γιατί"
+MAIN_NAME_PATTERN_PEOPLE = "_ποιος"
+
+MAIN_RUN_APP_BUTTON = "Τρέχα αγόρι, τρέχα"
+
+# Info window
+INFO_WINDOW_TITLE = "Πληροφορίες Photonomist"
+
+INFO_TITLE = "Photonomist"
+
+INFO_AIM = "Photonomist aims at helping photo-lovers (or simply photo-owners) with tidying their photos. \n\nGiven a path that contains photos, photonomist will: \n  - extract the dates of your photos, \n  - create 'date' directories \n  - group photos according to their dates"
+
+INFO_NAME_TITLE = "It took its name from the words:"
+INFO_NAME = "Photo..   --> Photography (art of captruring the light:: Greek root: (Φως) Φωτογραφία)\n..nomist  --> Taxonomist  (person who groups entities into categories:: Greek root: Ταξινομία ή Ταξινόμηση)"
+
+INFO_GITHUB_LINK = "Click me.. "
+INFO_GITHUB_TEXT = " for Motivation, Features and Development Notes. I definitely deserve your time :D !!"
+INFO_GITHUB_URL = "https://github.com/panos23kar/Photonomist/blob/master/README.rst"
+
+INFO_EMAIL_GOALS = "  - ask for extra functionality \n  - report errors \n  - send your endless love"
+INFO_EMAIL = "photonomist.23@gmail.com"
+
+# Exclude window
+EXCl_TITLE = "Φάκελοι φωτ/ιών"
+EXCL_NUMBER_PHOTOS_LABEL = " photos in the folders below!\nUncheck the folders that you don't want me to touch!\n"
+EXCL_LINK = "άνοιξε άκελο"
+EXCL_RUN_BUTTON = "Έτοιμος"
+
+# Loading window
+LOAD_TITLE = "Το δουλεύω! Περιμένε"
