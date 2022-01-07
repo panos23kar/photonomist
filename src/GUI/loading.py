29ab9463ec88eb3be1a854c4e9083fe5b29e392d
panos23kar <panos23kar@gmail.com>
2022-01-07 17:18:42 +0100

    __init__ for loading window

--- src/GUI/loading.py
+++ src/GUI/loading.py
@@ -7,3 +7,11 @@ class Loading:
     Draws the Loading window
     |
     """
+
+    def __init__(self, main_window) -> None:
+        """
+        Initializes the loading window
+        |
+        """
+        self.main_window = main_window
+        self.__angle = 0
