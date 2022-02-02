376fcd1d4bd58b2592d10514b77f0925cc80f297
panos23kar <panos23kar@gmail.com>
2022-02-02 12:28:38 +0100

    trial

--- doc/conf.py
+++ doc/conf.py
@@ -14,6 +14,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 #sys.path.insert(0,r'C:\repos\photonomist\src\photonomist')
+#Panos
+print("argument {}".format(sys.argv[1]))
 
 # -- Project information -----------------------------------------------------
 
