0fd6de780a085d90ad78cd74ae56f55ebd08c96b
panos23kar <panos23kar@gmail.com>
2022-02-13 17:54:08 +0100

    translate to greek the info window

--- src/GUI/languages/gr.py
+++ src/GUI/languages/gr.py
@@ -22,7 +22,7 @@ MAIN_MONTH_RADIO_BUTTON_VALUE = "μήνα"
 MAIN_YEAR_RADIO_BUTTON = "Χρόνο"
 MAIN_YEAR_RADIO_BUTTON_VALUE = "χρόνο"
 
-MAIN_NAME_PATTERN_LABEL = "Eπιλέξτε τις ετικέτες που θέλετε να προσθέσετε στο όνομα των φακέλων σας:"
+MAIN_NAME_PATTERN_LABEL = "Eπέλεξε τις ετικέτες που θέλεις να προσθέσεις στο όνομα των φακέλων:"
 MAIN_NAME_PATTERN_PLACE = "_που"
 MAIN_NAME_PATTERN_REASON = "_γιατί"
 MAIN_NAME_PATTERN_PEOPLE = "_ποιος"
@@ -30,30 +30,29 @@ MAIN_NAME_PATTERN_PEOPLE = "_ποιος"
 MAIN_RUN_APP_BUTTON = "Τρέχα αγόρι, τρέχα"
 
 # Info window
-INFO_WINDOW_TITLE = "Πληροφορίες Photonomist"
+INFO_WINDOW_TITLE = "Πληροφορίες για τον κ. Photonomist"
 
 INFO_TITLE = "Photonomist"
 
-INFO_AIM = "Photonomist aims at helping photo-lovers (or simply photo-owners) with tidying their photos. \n\nGiven a path that contains photos, photonomist will: \n  - extract the dates of your photos, \n  - create 'date' directories \n  - group photos according to their dates"
+INFO_AIM = "Ο Photonomist στοχεύει στο να βοηθήσει τους λάτρεις της φωτογραφίας (ή απλά τους κατόχους \nφωτογραφιών) να τακτοποιήσουν τις φωτογραφίες τους. \n\n Αν του δώσεις έναν φάκελο που περιέχει φωτογραφίες, ο photonomist: \n - θα εξαγάγει τις ημερομηνίες των φωτογραφιών σου, \n - θα δημιουργήσει φακέλους ' ημερομηνιών ' \n - θα τις ομαδοποιήσει σύμφωνα με τις ημερομηνίες τους"
 
-INFO_NAME_TITLE = "It took its name from the words:"
-INFO_NAME = "Photo..   --> Photography (art of captruring the light:: Greek root: (Φως) Φωτογραφία)\n..nomist  --> Taxonomist  (person who groups entities into categories:: Greek root: Ταξινομία ή Ταξινόμηση)"
+INFO_NAME_TITLE = "Πήρε το όνομά του από τις λέξεις:"
+INFO_NAME = "Photo..   --> Photography (η τέχνη της σύλληψης του φωτός:: Ρίζα: (Φως) Φωτογραφία)\n..nomist  --> Taxonomist  (πρόσωπο που ομαδοποιεί οντότητες σε κατηγορίες:: Ρίζα: Ταξινομία ή Ταξινόμηση)"
 
-INFO_GITHUB_LINK = "Click me.. "
-INFO_GITHUB_TEXT = " for Motivation, Features and Development Notes. I definitely deserve your time :D !!"
-INFO_GITHUB_URL = "https://github.com/panos23kar/Photonomist/blob/master/README.rst"
+INFO_GITHUB_LINK = "Click me.."
+INFO_GITHUB_TEXT = "για να μάθεις 'γιατί φτιάχτηκε', χαρακτηριστικά και τεχνικές λεπτομέρειες.Σίγουρα αξίζω τον χρόνο σου!!"
 
-INFO_EMAIL_GOALS = "  - ask for extra functionality \n  - report errors \n  - send your endless love"
+INFO_EMAIL_GOALS = " - ζήτησε επιπλέον λειτουργικότητα \n - ανέφερε σφάλματα και \n - στείλε την αγάπη σου"
 INFO_EMAIL = "photonomist.23@gmail.com"
 
 # Exclude window
 EXCl_TITLE = "Φάκελοι φωτ/ιών"
-EXCL_NUMBER_PHOTOS_LABEL = " photos in the folders below!\nUncheck the folders that you don't want me to touch!\n"
+EXCL_NUMBER_PHOTOS_LABEL = " Βρήκα φωτογραφίες στους παρακάτω φακέλους!\n Αποεπέλεξε (πςςςςς) τους φακέλους που δεν θέλεις να αγγίξω!\n"
 EXCL_LINK = "link"
 EXCL_RUN_BUTTON = "Έτοιμος"
 
 # Loading window
-LOAD_TITLE = "Το δουλεύω! Περιμένε"
+LOAD_TITLE = "Το δουλεύω! Περίμενεεεεεεε"
 
 # Exceptions
 NOT_VALID_PATH = "Ο φάκελος δεν υπάρχει!"
